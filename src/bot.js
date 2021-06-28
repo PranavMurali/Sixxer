b7f216eb91286f1abdd734de2f6ae8ff936c63ed
Pranav Murali <pranavmurali2001@gmail.com>
2021-06-28 14:41:59 +0530

    :baby_chick: basics and webhooks

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 
-const { Client } = require('discord.js');
-const client = new Client();
+const { Client, WebhookClient } = require('discord.js');
+const client = new Client({
+    partials:['MESSAGE','REACTION']
+});
 const PREFIX="..";
+const webhookClient= new WebhookClient(
+    process.env.WEBHOOK_ID,
+    process.env.WEBHOOOK_TOKEN,
+);
 
 client.on('ready', ()=> {
     console.log(`${client.user.tag} has logged in.`);
@@ -24,10 +30,40 @@ client.on('message', (message)=>{
         console.log(args)
 
         if(CMD_NAME=="kick"){
-            const member = message.guild.members.cache;
-            console.log(member)
+            if(args.length==0) return message.reply("Please provide an id.");
+            if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply("you dont have permission");
+            const member = message.guild.members.cache.get(args[0]);
+            if(member){
+                member
+                .kick()
+                .then((member)=>message.channel.send(`${member} was removed`))
+                .catch((err)=>message.channel.send("no permission"));
+
+            }
+            else{
+                message.channel.send("Member not exists");
+            }
+        }
+        else if (CMD_NAME=="ann"){
+            const msg=args.join(' ');
+            webhookClient.send(msg)
+            .catch((err)=>message.channel.send("no permission"));
         }
     }
 });
 
+client.on("messageReactionAdd",(reaction, user,message)=> {
+    const {name} =reaction.emoji;
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === "858987926163554314"){
+        switch(name){
+            case '😃':
+                member.roles
+                .add('750313998951383141')
+                .catch((err)=>console.log("asd"));
+                console.log(name);
+                break;
+        }
+    }
+});
 client.login(process.env.BOT_TOKEN)
